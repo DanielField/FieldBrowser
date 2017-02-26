5c4a6e01f51223ae1b7d5d8c5db39f6d0daa296c
Daniel Field <danielfield3515@gmail.com>
2017-02-26 12:57:35 +1000

    First commit. Options tab not yet implemented

---
+++ FieldBrowser/FrmMain.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace FieldBrowser
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabs
+            // 
+            this.tabs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabs.Controls.Add(this.tabPage1);
+            this.tabs.Controls.Add(this.tabPage2);
+            this.tabs.Location = new System.Drawing.Point(0, 36);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(1058, 502);
+            this.tabs.TabIndex = 9;
+            this.tabs.SelectedIndexChanged += new System.EventHandler(this.tabs_SelectedIndexChanged);
+            this.tabs.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tabs_MouseClick);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1050, 476);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1033, 468);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnOptions
+            // 
+            this.btnOptions.Location = new System.Drawing.Point(164, 7);
+            this.btnOptions.Name = "btnOptions";
+            this.btnOptions.Size = new System.Drawing.Size(23, 23);
+            this.btnOptions.TabIndex = 17;
+            this.btnOptions.Text = "O";
+            this.btnOptions.UseVisualStyleBackColor = true;
+            this.btnOptions.Click += new System.EventHandler(this.btnOptions_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(135, 7);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(23, 23);
+            this.btnStop.TabIndex = 16;
+            this.btnStop.Text = "S";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(106, 7);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(23, 23);
+            this.btnHome.TabIndex = 15;
+            this.btnHome.Text = "H";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(77, 7);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(23, 23);
+            this.btnRefresh.TabIndex = 14;
+            this.btnRefresh.Text = "R";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(48, 7);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(23, 23);
+            this.btnForward.TabIndex = 13;
+            this.btnForward.Text = ">";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(19, 7);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(23, 23);
+            this.btnBack.TabIndex = 12;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(1008, 7);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(29, 23);
+            this.btnGo.TabIndex = 11;
+            this.btnGo.Text = ">>";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAddress.Location = new System.Drawing.Point(193, 9);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(809, 20);
+            this.txtAddress.TabIndex = 10;
+            this.txtAddress.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtAddress_KeyUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 536);
+            this.Controls.Add(this.tabs);
+            this.Controls.Add(this.btnOptions);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtAddress);
+            this.Name = "FrmMain";
+            this.Text = "Field Browser";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnOptions;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FieldBrowser/FrmMain.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FieldBrowser
+{
+    public partial class FrmMain : Form
+    {
+        string home = "google.com";
+        List<WebKit.WebKitBrowser> pages = new List<WebKit.WebKitBrowser>();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            newBrowser();
+            tabs.TabPages[0].Text = "New tab";
+            tabs.TabPages[1].Text = "+";
+        }
+
+        private void LoadOptions()
+        {
+            // Load tab
+            WebKit.WebKitBrowser browser = new WebKit.WebKitBrowser();
+            pages.Insert(pages.Count - 1, browser);
+            TabPage options = new TabPage("Options");
+            tabs.TabPages.Insert(pages.Count - 1, options);
+            tabs.SelectedTab = options;
+            
+            // Load content
+                
+        }
+
+        private void newBrowser()
+        {
+            WebKit.WebKitBrowser browser = new WebKit.WebKitBrowser();
+            browser.Show();
+            browser.Dock = DockStyle.Fill;
+            pages.Add(browser);
+            tabs.SelectedTab.Controls.Add(pages[pages.Count - 1]);
+
+            pages[pages.Count - 1].Navigate(home);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            GetBrowserControl().Navigate(txtAddress.Text);
+        }
+
+        private WebKit.WebKitBrowser GetBrowserControl()
+        {
+            try { return (pages[tabs.SelectedIndex]); }
+            catch { return new WebKit.WebKitBrowser(); }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (GetBrowserControl().CanGoBack)
+                GetBrowserControl().GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if(GetBrowserControl().CanGoForward)
+                GetBrowserControl().GoForward();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            GetBrowserControl().Reload(WebBrowserRefreshOption.Completely);
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            GetBrowserControl().Navigate(home);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            GetBrowserControl().Stop();
+        }
+
+        private void btnOptions_Click(object sender, EventArgs e)
+        {
+            LoadOptions();
+        }
+
+        private void txtAddress_KeyUp(object sender, KeyEventArgs e)
+        {
+            e.SuppressKeyPress = true;
+            e.Handled = true;
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                GetBrowserControl().Navigate(txtAddress.Text);
+                GetBrowserControl().Select();
+            }
+        }
+
+        private void tabs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabs.SelectedIndex == tabs.TabCount - 1)
+            {
+                tabs.TabPages.Add("+");
+                tabs.SelectedTab.Text = "New tab";
+                newBrowser();
+                GetBrowserControl().Navigate(home);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // For each tab, update the title to display the title of the document
+            for (int i = 0; i < tabs.TabPages.Count - 1; i++)
+            {
+                if (!string.IsNullOrEmpty(pages[i].DocumentTitle) && tabs.TabPages[i].Text != "Options")
+                {
+                    tabs.TabPages[i].Text = pages[i].DocumentTitle;
+                }
+            }
+
+            // Grey out buttons where necessary
+            if (!GetBrowserControl().CanGoBack)
+                btnBack.Enabled = false;
+            else
+                btnBack.Enabled = true;
+
+            if (!GetBrowserControl().CanGoForward)
+                btnForward.Enabled = false;
+            else
+                btnForward.Enabled = true;
+        }
+
+        private void tabs_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Button == MouseButtons.Middle)
+                {
+                    tabs.SelectedIndex = tabs.SelectedIndex - 1;
+                    tabs.TabPages.RemoveAt(tabs.SelectedIndex + 1);
+                    pages.RemoveAt(tabs.SelectedIndex + 1);
+                }
+            }catch{}
+        }
+    }
+}
